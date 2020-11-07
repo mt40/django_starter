da27dcd4bdccf97d3ce87d3a9a8481b001a6618b
Minh Thai <minhthai40@gmail.com>
2020-11-07 16:57:15 +0700

    add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,38 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+# pylint: disable=too-few-public-methods
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status="published")
+
+
+class Post(models.Model):
+    class Meta:
+        ordering = ("-publish",)
+
+    # The default manager
+    objects = models.Manager()
+
+    # Our custom manager. Use it by calling `Post.published.???`
+    published = PublishedManager()
+
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+    title = models.CharField(max_length=250, blank=False)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+
+    def __str__(self):
+        return self.title  # pylint: disable=invalid-str-returned
