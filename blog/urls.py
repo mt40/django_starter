e98df767995b36dbb06cee58c69cde22ff0b37f1
Minh Thai <minhthai40@gmail.com>
2020-11-08 11:12:20 +0700

    support sharing by email

--- blog/urls.py
+++ blog/urls.py
@@ -7,10 +7,11 @@ app_name = "blog"  # pylint: disable=invalid-name
 
 urlpatterns = [
     # post views
-    path('', views.PostListView.as_view(), name='post_list'),
+    path("", views.PostListView.as_view(), name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/",
         views.post_detail,
         name="post_detail",
     ),
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -42,6 +44,32 @@ class PostListView(ListView):
     """
 
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
+
+    if request.method == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            # retrieve form fields passed validation
+            data = form.cleaned_data
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+
+            subject = f"""{data['name']} ({data['email']}) recommends you reading '{post.title}'"""
+            message = f'Read "{post.title}" at {post_url}\n\n' \
+                      f'{data["name"]}\'s comments: {data["comments"]}'
+            send_mail(subject, message, "admin@myblog.com", [data["to"]])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
